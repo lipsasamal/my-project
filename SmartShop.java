6bf3978deb3f78e4a5f52270a8f4fbcaeaebe94c
Lipsa Samal <lipsasamal1010@gmail.com>
2026-03-27 15:46:48 +0530

    Add files via upload

---
+++ SmartShop.java
@@ -0,0 +1,43 @@
+package com.nit.ls.servletcontext;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+ 
+public class SmartShop extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public SmartShop() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		
+		ServletContext context= request.getServletContext();
+		 String WebsiteName=context.getInitParameter("websiteName");
+		 String  CompanyName=context.getInitParameter("CompanyName");
+		 String CustomerCare=context.getInitParameter("CustomerCare");
+		 
+		 out.println("Website Name : "+WebsiteName);
+		 out.println("Company Name : "+CompanyName);
+		 out.println("Customer Care : "+CustomerCare);
+		 
+	 
+	}
+
+	 
+	 
+
+}
